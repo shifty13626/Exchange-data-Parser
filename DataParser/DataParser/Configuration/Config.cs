59e115cd8ef2d28b42880b3f4e0f64686f843422
Ludo <shifty13626@gmail.com>
2021-01-13 15:13:25 +0100

    First request + subject projet
    First request : Info/search game

---
+++ DataParser/DataParser/Configuration/Config.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataParser.Configuration
+{
+    public class Config
+    {
+        /// <summary>
+        /// Address of API
+        /// </summary>
+        public string AddressAPI { get; set; }
+        /// <summary>
+        /// Token to connect to API
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// ClientID identifier to connect to API
+        /// </summary>
+        public string ClientID { get; set; }
+    }
+}
---
+++ DataParser/DataParser/Configuration/ConfigLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataParser.Configuration
+{
+    /// <summary>
+    /// Manager to load configuration
+    /// </summary>
+    public class ConfigLoader
+    {
+        #region - Nodes
+        // All nodes XML file
+        private static string Node_Config = "config";
+        private static string Node_AddresseAPI = "addressAPI";
+        private static string Node_TokenAPI = "token";
+        private static string Node_ClientIdAPI = "clientID";
+        #endregion
+
+
+        /// <summary>
+        /// Load data from XML File
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public Config LoadConfig(string url)
+        {
+            var doc = XDocument.Load(url);
+            var root = doc.Element(Node_Config);
+            if (root == null)
+            {
+                Console.WriteLine("Can't find root element of config");
+                return null;
+            }
+
+            return new Config()
+            {
+                AddressAPI = root.Element(Node_AddresseAPI).Value,
+                Token = root.Element(Node_TokenAPI).Value,
+                ClientID = root.Element(Node_ClientIdAPI).Value
+            };
+        }
+    }
+}
